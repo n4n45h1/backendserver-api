56a78ea42b1eab18f57b5df0274a262dec3842e5
Nanashi <a@techz.jp>
2025-02-26 23:52:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors'); // corsミドルウェアの追加
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors()); // すべてのオリジンからのリクエストを許可
 
 app.use(express.json());
 
